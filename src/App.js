5987c595388c0ea98a68b59e286a2ffc6b685ec5
Juliano Pereira Lima <juliano@qualyteam.com.br>
2019-07-24 16:54:21 -0300

    :art: Remove unused vars

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import React, { Fragment } from "react";
 import "./App.scss";
 import { Navbar } from "./components/navbar";
 import { Login } from "./pages/login";
-import { Register } from "./pages/register";
 import { List } from "./pages/list";
-import { Recipe } from "./components/Recipe";
-
-const recipe = {
-  id: 6,
-  title: "Batata belga",
-  description: "Uma boa batata",
-  imageUrl:
-    "https://static.baratocoletivo.com.br/2017/0622/10012991/g_allfry-c4bbebed11.jpg",
-  ingredients: "Batata, óleo, sal.",
-  preparo: "Coloque tudo na fritadeira e uhaaa!"
-};
 
 function App() {
   return (
@@ -24,8 +12,6 @@ function App() {
           <Navbar />
           <main className="content--container">
             <List />
-            {/* <Recipe {...recipe} /> */}
-            {/* <Register /> */}
           </main>
         </Fragment>
       ) 
